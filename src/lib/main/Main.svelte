8f681daf88a89ed159a3eb84fc6dc762dfae7f61
BirdyArt <asobolev91@gmail.com>
2021-04-13 16:32:17 -0700

    icons added

--- src/lib/main/Main.svelte
+++ src/lib/main/Main.svelte
@@ -1,8 +1,25 @@
 <script>
   import { typewriter as typewriterAction } from "$utils/actions";
   import { onMount } from 'svelte';
+  import { fly } from 'svelte/transition';
 
   let visible = false;
+
+  let icons = [
+      { id: 1, src: "/icons/javascript.svg", alt: "javascript icon" },
+      { id: 2, src: "/icons/css-3.svg", alt: "css-3 icon" },
+      { id: 3, src: "/icons/html-5.svg", alt: "html-5 icon" },
+      { id: 4, src: "/icons/nodejs-icon.svg", alt: "nodejs icon" },
+      { id: 5, src: "/icons/react.svg", alt: "react icon" },
+      { id: 6, src: "/icons/redux.svg", alt: "redux icon" },
+      { id: 7, src: "/icons/graphql.svg", alt: "graphql icon" },
+      { id: 8, src: "/icons/svelte-icon.svg", alt: "svelte icon" },
+      { id: 9, src: "/icons/tailwindcss-icon.svg", alt: "tailwindcss icon" },
+      { id: 10, src: "/icons/sass.svg", alt: "sass icon" },
+      { id: 11, src: "/icons/bootstrap.svg", alt: "bootstrap icon" },
+      { id: 12, src: "/icons/mongodb.svg", alt: "mongodb icon" },
+      { id: 13, src: "/icons/fauna.svg", alt: "faunadb icon" }
+    ];
 
   onMount(() => visible = true);
 </script>
@@ -10,12 +27,17 @@
 {#if visible}
   <div class="grid grid-flow-col grid-cols-1 grid-rows-2 md:grid-cols-3 md:grid-rows-1 gap-4 px-8 2xl:px-80 xl:pl-64 lg:pl-48 lg:pr-24 md:pl-36 md:pt-16">
     <div class="text-center pt-10 lg:pt-16 col-span-2">
-      <p class="text-4xl" in:typewriterAction={{ speed: 50, delay: 1000 }}>
-        Hi there, I'm Artem Sobolev aka BirdyArt 👋	
+      <p class="text-4xl" in:typewriterAction={{ speed: 70, delay: 1000 }}>
+        Hi! I'm Artem Sobolev aka BirdyArt
       </p>
-      <p class="text-xl pt-10" in:typewriterAction={{ speed: 40, delay: 4000 }}>
-        I am a Frontend Developer skilled in JavaScript, Svelte, React, Redux, Svelte, Node.js, HTML5, CSS3, SaSS, Mongodb, FaunaDB.
+      <p class="text-2xl pt-10" transition:fly="{{ x: 100, delay: 3000, duration: 2000 }}">
+        Frontend Developer skilled in:
       </p>
+      <div class="flex flex-wrap items-center justify-evenly pt-6">
+        {#each icons as icon,i}
+          <img src={icon.src} alt={icon.alt} width="30" class="m-2" transition:fly="{{ x: 100, delay: 3000+i*200, duration: 2000 }}" />
+        {/each}
+      </div>
     </div>
     <img src="/images/profile.webp" alt="profile pic" width="300" class="rounded-full place-self-center" />
   </div>
